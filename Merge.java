5a8b05fcb880e6e372ff070a7313ecb65fdebf0a
Tejas Narayan <tnarayan00@stuy.edu>
2019-03-17 14:38:32 -0400

    optimized merge works, but is slow

--- Merge.java
+++ Merge.java
@@ -2,51 +2,48 @@ import java.util.Arrays;
 
 public class Merge {
   public static void mergesort(int[] data) {
-    sort(data, 0, data.length - 1);
+    sort(data, new int[data.length], 0, data.length - 1);
   }
 
-  private static void sort(int[] data, int lo, int hi) {
+  private static void sort(int[] data, int[] temp, int lo, int hi) {
     //System.out.println(Arrays.toString(data) + " lo = "+lo+", hi = "+hi);
     if (lo >= hi) {
       return;
     }
     int mid = lo + (hi - lo) / 2;
-    sort(data, lo, mid);
-    sort(data, mid + 1, hi);
-    merge(data, lo, mid, hi);
+    sort(data, temp, lo, mid);
+    sort(data, temp, mid + 1, hi);
+    merge(data, temp, lo, mid, hi);
   }
 
-  private static void merge(int[] data, int lo, int mid, int hi) {
+  private static void merge(int[] data, int[] temp, int lo, int mid, int hi) {
     //System.out.println("\tMerging "+ Arrays.toString(data)+" at "+lo+","+mid+","+hi);
     int[] data1 = new int[mid-lo+1];
     int[] data2 = new int[hi-mid];
 
-    for (int i = 0; i < data1.length; i++) {
-      data1[i] = data[lo+i];
-    }
-    for (int i = 0; i < data2.length; i++) {
-      data2[i] = data[mid+i+1];
+    for (int i = 0; i <= hi-lo; i++) {
+      temp[lo+i] = data[lo+i];
     }
 
     int i = 0;
     int j = 0;
     while (i < mid-lo+1 && j < hi-mid) {
-      if (data1[i] < data2[j]) {
-        data[lo+i+j] = data1[i];
+      if (temp[lo+i] < temp[mid+j+1]) {
+        data[lo+i+j] = temp[lo+i];
         i++;
       } else {
-        data[lo+i+j] = data2[j];
+        data[lo+i+j] = temp[mid+j+1];
         j++;
       }
-      //System.out.println("\t"+Arrays.toString(data1)+","+Arrays.toString(data2)+" i="+i+",j="+j);
+      //System.out.println("\t"+Arrays.toString(temp)+" i="+i+",j="+j);
     }
 
     while (i < mid-lo+1) {
-      data[lo+i+j] = data1[i];
+      data[lo+i+j] = temp[lo+i];
       i++;
     }
     while (j < hi-mid) {
-      data[lo+i+j] = data2[j];
+      data[lo+i+j] = temp[mid+j+1];
       j++;
     }
   }
