1e56d212d838b6fcdb60d1f75b4767450a1a68cc
Tejas Narayan <tnarayan00@stuy.edu>
2019-03-17 14:47:05 -0400

    reduced optimized time slightly

--- Merge.java
+++ Merge.java
@@ -2,7 +2,12 @@ import java.util.Arrays;
 
 public class Merge {
   public static void mergesort(int[] data) {
-    sort(data, new int[data.length], 0, data.length - 1);
+    int[] temp = new int[data.length];
+    for (int i = 0; i < temp.length; i++) {
+      temp[i] = data[i];
+    }
+
+    sort(data, temp, 0, data.length - 1);
   }
 
   private static void sort(int[] data, int[] temp, int lo, int hi) {
@@ -17,17 +22,12 @@ public class Merge {
   }
 
   private static void merge(int[] data, int[] temp, int lo, int mid, int hi) {
-    //System.out.println("\tMerging "+ Arrays.toString(data)+" at "+lo+","+mid+","+hi);
-    int[] data1 = new int[mid-lo+1];
-    int[] data2 = new int[hi-mid];
-
-    for (int i = 0; i <= hi-lo; i++) {
-      temp[lo+i] = data[lo+i];
-    }
+    //System.out.println("\tMerging "+ Arrays.toString(temp)+" at "+lo+","+mid+","+hi);
 
     int i = 0;
     int j = 0;
     while (i < mid-lo+1 && j < hi-mid) {
+      //System.out.println("\t"+Arrays.toString(data)+" i="+i+",j="+j);
       if (temp[lo+i] < temp[mid+j+1]) {
         data[lo+i+j] = temp[lo+i];
         i++;
@@ -35,8 +35,8 @@ public class Merge {
         data[lo+i+j] = temp[mid+j+1];
         j++;
       }
-      //System.out.println("\t"+Arrays.toString(temp)+" i="+i+",j="+j);
     }
+    //System.out.println("\t"+Arrays.toString(temp)+" i="+i+",j="+j);
 
     while (i < mid-lo+1) {
       data[lo+i+j] = temp[lo+i];
@@ -46,5 +46,7 @@ public class Merge {
       data[lo+i+j] = temp[mid+j+1];
       j++;
     }
+
+    for (int k = lo; k <= hi; k++) temp[k] = data[k];
   }
 }
